7919d0c17476d6639fa13b7091e0e11776df17c7
Renan Pinho <pinhorenan@icloud.com>
2023-02-15 13:42:37 -0300

    refatoração

--- lista3_AP/ex10.c
+++ lista3_AP/ex10.c
@@ -3,13 +3,13 @@ quadrante ao qual o ponto pertence. Se o ponto estiver sobre os eixos, ou na ori
 EM NENHUM QUADRANTE. Considere que o usuário poderá informar qualquer valor para as
 coordenadas.
 
-[Entrada]| [Entrada]| [Entrada]| [Entrada]
-2 (x)    | -8 (x)   | -4 (x)   | 6 (x)
-3 (y)    | 5 (y)    | -3 (y)   | -2 (y)
-         |          |          |
-[Saída]  | [Saída]  | [Saída]  | [Saída]
-I        | II       | III      | IV
----------+-------+--+----------+---+--------------------
+[Entrada]        | [Entrada]       | [Entrada]  | [Entrada]
+2 (x)            | -8 (x)          | -4 (x)     | 6 (x)
+3 (y)            | 5 (y)           | -3 (y)     | -2 (y)
+                 |                 |            |
+[Saída]          | [Saída]         | [Saída]    | [Saída]
+I                | II              | III        | IV
+-----------------+-----------------+------------+-------
 [Entrada]        | [Entrada]       | [Entrada]
 0 (x)            | 8 (x)           | 0 (x)
 0 (y)            | 0 (y)           | -2 (y)
